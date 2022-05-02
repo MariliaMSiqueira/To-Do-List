0cf1d8c74d3673bc9ee960fbe564a3025a2a3beb
MariliaMSiqueira <mariliamatiassiqueira98@gmail.com>
2022-05-02 14:30:11 -0300

    adding features

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,17 +14,65 @@ class Tasks {
 }
 
 // RESPONSÁVEL PELA TRATATIVA DAS TAREFAS NO LOCALSTORAGE
-class Bd {}
+class Bd {
+  constructor() {
+    let firstId = localStorage.getItem("id");
+
+    if (firstId === null || firstId === "" || firstId === undefined) {
+      firstId = localStorage.setItem("id", 0);
+    }
+  }
+  // getAllTasks() {
+  //   let arrTasks = [];
+  //   let id = localStorage.getItem("id");
+  //   for (let i = 1; i <= id; i++) {
+  //     let listTasks = JSON.parse(localStorage.getItem(i));
+
+  //     if (listTasks === null) {
+  //       continue;
+  //     }
+
+  //     listTasks.id = i;
+
+  //     arrTasks.push(listTasks);
+  //   }
+  //   return arrTasks;
+  // }
+
+  // PARÂMETRO INPUTVALUE PASSADO NA INSTÂNCIA DO OBJETO
+  pinStorage(info) {
+    let lengthId = localStorage.length;
+    localStorage.setItem(lengthId + 1, JSON.stringify(info));
+    localStorage.refresh;
+    let id = localStorage.getItem("id");
+    let tasksList = document.getElementById("listTasks");
+    let row = tasksList.insertRow(info);
+    row.insertCell(0).innerHTML = info.tasks;
+
+    let btn = document.createElement("button");
+    btn.className = "btn btn-success";
+    btn.innerHTML = '<i class="fas fa-check"></i>';
+    btn.setAttribute("id", id);
+
+    btn.onclick = () => {
+      if (btn.getAttribute("id") === info.id) {
+        tasksList.style.textDecoration = "line-through";
+      }
+    };
+    row.insertCell(1).append(btn);
+  }
+}
+let bd = new Bd();
 
 // BUTTON CADASTRAR TAREFAS
 // AO CADASTRAR UM TAREFA SERÁ INSTANCIADA UMA CLASSE QUE, POR SUA VEZ, HAVERÁ UMA VALIDAÇÃO
 function registerTasks() {
-  let tasksInput = document.getElementById("tasks").value;
+  let tasksInput = document.getElementById("tasks");
   let modalTitle = document.getElementById("modal-title");
   let modalBody = document.getElementById("modal-body");
   let modalBtn = document.getElementById("modal-btn");
 
-  let tasks = new Tasks(tasksInput);
+  let tasks = new Tasks(tasksInput.value);
 
   if (tasks.validateTasks()) {
     modalTitle.innerHTML = "Cadastrado com sucesso!";
@@ -34,6 +82,8 @@ function registerTasks() {
     modalBtn.className = "btn btn-success";
     $("#dialogModal").modal("show");
     resetInfo();
+    // ENVIANDO O OBJETO COMO PARÂMETRO
+    bd.pinStorage(tasks);
   } else {
     modalTitle.innerHTML = "Erro no cadastro!";
     modalTitle.className = "modal-title text-danger";
